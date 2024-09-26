4fbb9bdc281a90fea7d3203c665afd36d5926178
Matheus A. <matheusaugusto28azevedo@gmail.com>
2024-09-26 18:20:53 -0300

    Create sorteiaTimes.py

---
+++ sorteiaTimes.py
@@ -0,0 +1,34 @@
+import random
+
+# Lista de jogadores com suas notas (nome, nota)
+jogadores = [
+    ("Matheus", 70), ("Mayquin", 70), ("Jimmy", 80), ("Biel", 70),
+    ("Pablo", 65), ("André", 60), ("Guilherme Caldas", 60), ("Marley", 65),
+    ("Fábio", 60), ("Ferrari", 50), ("Caio", 60), ("Wanderson", 55),
+    ("Adriano", 45), ("Marco thúlio", 45), ("Chico", 65), ("Marcelin", 65)
+]
+# Função para balancear os times
+def balancear_times(jogadores):
+    # Ordenar os jogadores pela nota (do maior para o menor)
+    jogadores.sort(key=lambda x: x[1], reverse=True)
+    
+    # Inicializar times
+    times = [[], [], [], []]
+
+    # Alternar jogadores entre os 4 times para balancear
+    for i, jogador in enumerate(jogadores):
+        times[i % 4].append(jogador)
+
+    return times
+
+# Função para calcular o total de pontos de um time
+def calcular_total(time):
+    return sum([jogador[1] for jogador in time])
+
+# Sorteia times balanceados
+times = balancear_times(jogadores)
+
+# Exibe os times e seus totais
+for i, time in enumerate(times):
+    print(f"Time {i + 1}: {time}")
+    print(f"Total de pontos do Time {i + 1}: {calcular_total(time)}\n")
